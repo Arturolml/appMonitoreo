b89888a8b1f77d81031e3327fed458496927b620
Arturolml <ar2ro.lml@gmail.com>
2026-03-16 17:03:05 -0600

    arreglos en schema4

---
+++ backMonitoreo/prisma/seed.js
@@ -0,0 +1,70 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const client_1 = require("@prisma/client");
+const bcrypt = __importStar(require("bcryptjs"));
+const prisma = new client_1.PrismaClient();
+async function main() {
+    const existingAdmin = await prisma.usuario.findUnique({
+        where: { email: 'admin@admin.com' },
+    });
+    if (!existingAdmin) {
+        const saltOrRounds = 10;
+        const hashedPassword = await bcrypt.hash('admin123', saltOrRounds);
+        await prisma.usuario.create({
+            data: {
+                email: 'admin@admin.com',
+                password: hashedPassword,
+                nombre: 'Admin',
+                apellidoPaterno: 'Sistema',
+                rol: 'ADMIN',
+            },
+        });
+        console.log('✅ Usuario Administrador Creado: admin@admin.com / admin123');
+    }
+    else {
+        console.log('ℹ️ El usuario administrador ya existe. Omitiendo creación.');
+    }
+}
+main()
+    .catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
+    .finally(async () => {
+    await prisma.$disconnect();
+});
+//# sourceMappingURL=seed.js.map
